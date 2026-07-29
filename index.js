5912d1ad482fba86ce2f80b9e3983b16d6075eda
syahrulauliar <67372456+syahrulauliar@users.noreply.github.com>
2026-07-29 09:02:33 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+console.clear();
+
+console.log("======================================");
+console.log("🚀 WhatsApp Finance Bot");
+console.log("======================================");
+console.log("Bot Name :", process.env.BOT_NAME);
+console.log("Owner    :", process.env.OWNER_NAME);
+console.log("Timezone :", process.env.TIMEZONE);
+console.log("======================================");
+console.log("✅ Application Started");
